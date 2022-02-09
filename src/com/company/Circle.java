69c143defcf80a8cf246aa9d222bfb7651900b0c
KALYS'S LAPTOP <bekmenbetov@gmail.com>
2022-02-09 22:13:31 +0600

    Initial commit

---
+++ src/com/company/Circle.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Circle {
+    static double pi= Math.PI;
+
+    public static String area(double radius){
+        return "Area of cicle is " + pi * (radius * radius);
+
+
+    }
+    public static String circumference(double radius){
+        return "circumference of circle is " +pi * 2 * radius;
+
+    }
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(Circle.area(3.6));
+        System.out.println(Circle.circumference(5));
+
+    }
+}
